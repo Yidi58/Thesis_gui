7d9611cf3c8592acf4041146f8d5f011b81ea214
Yidi58 <65209339+Yidi58@users.noreply.github.com>
2020-05-14 20:48:07 +1000

    Add files via upload
    Edit according to feedback

--- settle.py
+++ settle.py
@@ -1,32 +1,37 @@
 from tkinter import *
+from tkinter import messagebox
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import ImageTk, Image
 import time
 
-global count
-count = [0,0,0,0,0,0,0,0]
+## initiate global variables
+global electrode_status
+electrode_status = [0,0,0,0,0,0,0,0]  # check the status of the electrodes
 
-global customed_pulse
-customed_pulse = [[1,2],[3,4]]
+global customized_pulse
+customized_pulse = [[1,2],[3,4]]  # store the customized pulse
 
-global cus_duration
-cus_duration = 0
+global cus_duration 
+cus_duration = 0 # total duration of the customized pulse
 
 global cus_wid
-cus_wid = 0
-
-global send
-send = False
+cus_wid = 0  # the duration of each single customized pulse
 
 global start
-start = 0
+start = 0  # record starting time
 
+global send
+send = 0  # record the status, sending or not
+
+global stop
+stop = 0  # check stop button status
+
+## Design the pulse
 def designPulse():
-    global amplitude_input
     global duration_input
     global repeat_input
-    amp = int(amplitude_input.get())
+    amp = 1
     freq = int(repeat_input.get())
     dur = int(duration_input.get())
     D = np.linspace(0, dur,num=dur*10)
@@ -37,61 +42,85 @@ def designPulse():
     plt.plot(D,sig)
     plt.show()
 
+## Design customized pulse
 def customPulse():
-    global customed_pulse
+    global customized_pulse
     global cus_duration
     global cus_wid
-    cus_amp = float(custom_amp_input.get())
-    cus_wid = float(custom_width_input.get())
-    cus_ratio = float(custom_ratio_input.get())
-    cus_duration = float(custom_duration_input.get())
-    customed_signal_info = [cus_amp, cus_wid, cus_ratio, cus_duration]
-    if customed_signal_info[2] > 0:
-        customed_pulse[0][0] = customed_signal_info[0]*(customed_signal_info[2]/100)
-        customed_pulse[0][1] = customed_signal_info[1]*(100/customed_signal_info[2])
-        customed_pulse[1][0] = (-1)*customed_signal_info[0]
-        customed_pulse[1][1] = customed_signal_info[1]
-    if customed_signal_info[2] < 0:
-        customed_pulse[1][0] = customed_signal_info[0]*(customed_signal_info[2]/(100))
-        customed_pulse[1][1] = customed_signal_info[1]*(-100/customed_signal_info[2])
-        customed_pulse[0][0] = customed_signal_info[0]
-        customed_pulse[0][1] = customed_signal_info[1]
-    if customed_signal_info[2] == 0:
-        customed_pulse[0][0] = customed_signal_info[0]
-        customed_pulse[0][1] = customed_signal_info[1]
-        customed_pulse[1][0] = (-1)*customed_signal_info[0]
-        customed_pulse[1][1] = customed_signal_info[1]
-    
+    try:
+        cus_amp = float(custom_amp_input.get())
+        cus_wid = float(custom_width_input.get())
+        cus_ratio = float(custom_ratio_input.get())
+        cus_duration = float(custom_duration_input.get())
+        if cus_amp <= 0 or cus_wid <= 0 or cus_duration <= 0:
+            messagebox.showinfo("Error", "Cannot be zero or negative number")
+        customed_signal_info = [cus_amp, cus_wid, cus_ratio, cus_duration]
+        if customed_signal_info[2] > 0:
+            customized_pulse[0][0] = customed_signal_info[0]*(customed_signal_info[2]/100)
+            customized_pulse[0][1] = customed_signal_info[1]*(100/customed_signal_info[2])
+            customized_pulse[1][0] = (-1)*customed_signal_info[0]
+            customized_pulse[1][1] = customed_signal_info[1]
+        if customed_signal_info[2] < 0:
+            customized_pulse[1][0] = customed_signal_info[0]*(customed_signal_info[2]/(100))
+            customized_pulse[1][1] = customed_signal_info[1]*(-100/customed_signal_info[2])
+            customized_pulse[0][0] = customed_signal_info[0]
+            customized_pulse[0][1] = customed_signal_info[1]
+        if customed_signal_info[2] == 0:
+            customized_pulse[0][0] = customed_signal_info[0]
+            customized_pulse[0][1] = customed_signal_info[1]
+            customized_pulse[1][0] = (-1)*customed_signal_info[0]
+            customized_pulse[1][1] = customed_signal_info[1]
+    except ValueError:
+         messagebox.showinfo("Error", "Inappropriate input")
 
-   
+
+## Change electrode colour   
 def selectElectrode(num):
     global electrode_array
     global master
-    global count
+    global electrode_status
     index = num-1
-    if count[index] == 0:
+    if electrode_status[index] == 0:
         image_button = Image.open("orange.png")
-        count[index] = 1
-    elif count[index] != 0:
+        electrode_status[index] = 1
+    elif electrode_status[index] != 0:
         image_button = Image.open("electrode.png")
-        count[index] = 0
+        electrode_status[index] = 0
     new_img = image_button.resize((50,50))
     img = ImageTk.PhotoImage(new_img)
     electrode_array[index].config(image=img)
     master.mainloop()
 
-def sendSignal():
-
+## send message to the stimulator
+def sendSignal():  
+    global send
+    global stop
     global cus_duration
     global cus_wid
-    global customed_pulse    
-    start = time.time()
-    while((time.time()-start)*1000 < cus_duration):
-        master.after(10, master.update())
-        master.after(300, print(customed_pulse))
-        #customPulse()
+    global customized_pulse
+    global electrode_status
+    if electrode_status.count(1) != 2:
+        messagebox.showinfo("Error", "Choose exact two channels")
+    else:
+        if send == 0:
+            print(send)
+            start = time.time()
+            while((time.time()-start)*1000 < cus_duration) and stop == 0: # check stop button
+                send = 1  # record the sending status
+                master.after(10, master.update()) # update the interface
+                # control timing
+                # the number here is the interval between each customized pulse
+                master.after(300, print(customized_pulse)) 
+                print(send)
+                ##customPulse()
+            
+            send = 0  # change status after finishing sending the signal
+            stop = 0  # after stop button works, return its status back to zero
     
-    
+## stop sending message to the stimulator
+def stopSignal():
+    global stop
+    stop = 1
 
 master = Tk()
 signal = Frame(master, bg = "white")
@@ -113,14 +142,10 @@ repeat = Label(pulse_signal, text = "Repeat times: ", bg = "white")
 repeat.grid(row = 2, padx = 5)
 repeat_input = Entry(pulse_signal)
 repeat_input.grid(row = 2, column = 6)
-amplitude = Label(pulse_signal, text = "Pulse amplitude(mA): ", bg = "white")
-amplitude.grid(row = 3, padx = 5)
-amplitude_input = Entry(pulse_signal)
-amplitude_input.grid(row = 3, column = 6)
 duration = Label(pulse_signal, text = "Duration(ms): ", bg = "white")
-duration.grid(row = 4, padx = 5)
+duration.grid(row = 3, padx = 5)
 duration_input = Entry(pulse_signal)
-duration_input.grid(row = 4, column = 6)
+duration_input.grid(row = 3, column = 6)
 submit_button = Button(pulse_signal, text = "Finish", command = lambda: designPulse(), bg = "white")
 submit_button.grid(row = 6, padx = 5)
 ##custom_signal
@@ -144,7 +169,8 @@ custom_duration_input = Entry(custom_signal)
 custom_duration_input.grid(row=5, column=6, padx=5)
 custom_submit_button = Button(custom_signal, text = "Finish", command = lambda: customPulse(), bg = "white")
 custom_submit_button.grid(row=7, padx = 5)
-
+w = Scale(custom_signal, from_ = -100, to=100, orient = HORIZONTAL)
+w.grid(row = 8, padx = 5)
 
 ##electrode selection
 select_electrode = Label(electrode_pic, text = "Select electrodes", bg = "light yellow")
@@ -187,5 +213,9 @@ electrode_array = [electrode_button1,electrode_button2, electrode_button3,
 
 stimulation_button = Button(electrode_arrangement, text = "Stimulate", command = lambda: sendSignal(), bg = "white")
 stimulation_button.grid(row=3,column=30, padx=30)
+
+stop_button = Button(electrode_arrangement, text = "Stop", command = lambda: stopSignal(), bg = "white")
+stop_button.grid(row=4,column=30, padx=30)
+
 
 master.mainloop()
